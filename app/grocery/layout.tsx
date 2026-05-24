5df3dacd4d1ae5e39b78faaed12ffb2c2720368c
aminofabian <aminofab@gmail.com>
2026-05-25 00:20:20 +0300

    added

--- app/grocery/layout.tsx
+++ app/grocery/layout.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { AppShell } from "@/components/app-shell";
-import { GroceryNotificationListener } from "@/components/grocery/grocery-notification-listener";
 import { DashboardProvider } from "@/components/dashboard-provider";
 import { DashboardToaster } from "@/components/dashboard-sonner";
 import { RealtimeProvider } from "@/components/realtime-provider";
@@ -14,7 +13,6 @@ export default function GroceryLayout({ children }: GroceryLayoutProps) {
   return (
     <DashboardProvider>
       <RealtimeProvider>
-        <GroceryNotificationListener />
         <AppShell>{children}</AppShell>
         <DashboardToaster />
       </RealtimeProvider>
